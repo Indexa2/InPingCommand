4b94f7b695d121c4458037ec6dd144706e5b443b
[ASYNC] Squeeze <142709255+Indexa2@users.noreply.github.com>
2024-12-30 00:48:21 -0600

    Create Main.php

---
+++ InPingCommand/src/IndexDev/InPingCommand/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace IndexDev\InPingCommand;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase {
+
+    protected function onEnable(): void {
+        $this->getServer()->getCommandMap()->register("ping", new PingCommand("ping", "Check your ping", "/ping", ["p"], "inping.cmd"));
+    }
+}
+
+class PingCommand extends Command {
+
+    public function __construct(string $name, string $description = "", string $usageMessage = "", array $aliases = [], string $permission = "") {
+        parent::__construct($name, $description, $usageMessage, $aliases);
+        $this->setPermission("inping.cmd");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$this->testPermission($sender)) {
+            return true; 
+        }
+
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(TextFormat::RED . "This command can only be used in-game.");
+            return true;
+        }
+
+        $ping = $sender->getNetworkSession()->getPing();
+        $sender->sendMessage(TextFormat::GREEN . "Your ping: " . $ping . "ms");
+        return true;
+    }
+}
